077a744e1936cc96ae1bc7f18606de15f85b2832
Kyi Thar <khukt@users.noreply.github.com>
2024-05-23 18:00:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,16 +29,23 @@ def load_nlp_model():
 
 tokenizer, model = load_nlp_model()
 
+# Function to detect if a string contains Burmese characters
+def contains_burmese(text):
+    burmese_characters = set("ကခဂဃငစဆဇဈညဋဌဍဎဏတထဒဓနပဖဗဘမယရလဝသဟဠအဣဤဥဦဧဩဪါာိီုူေဲံ့းွှဿ၀၁၂၃၄၅၆၇၈၉")
+    return any(char in burmese_characters for char in text)
+
 # Function to parse the query and retrieve medicine information
 def parse_query(query):
+    query = query.lower()
+    search_in_english = any(char.isascii() for char in query)
+    search_in_burmese = contains_burmese(query)
+    results = []
+
     # Tokenize the query using the tokenizer
     inputs = tokenizer(query, return_tensors="pt")
     tokens = tokenizer.convert_ids_to_tokens(inputs["input_ids"].squeeze().tolist())
-
-    results = []
-
+    
     for med in medicines:
-        # Check for matches in all relevant fields
         generic_name_matches = any(token in med['generic_name'].lower() for token in tokens)
         generic_name_mm_matches = any(token in med.get('generic_name_mm', '').lower() for token in tokens)
         uses_matches = any(token in ' '.join(med['uses']).lower() for token in tokens)
