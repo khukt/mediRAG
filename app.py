26ca4088e1a0b50f67bb02ea9a8095e4cee1c8b8
Kyi Thar <khukt@users.noreply.github.com>
2024-05-29 23:59:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,100 +28,126 @@ generic_names = load_data('generic_names.json')['generic_names']
 manufacturers = load_data('manufacturers.json')['manufacturers']
 relationships = load_data('relationships.json')
 
-# Check for 'name' key in medicines and print debug information
-def check_key_presence(data, key, file_name):
-    missing_keys = [item for item in data if key not in item]
-    if missing_keys:
-        st.write(f"Missing '{key}' in the following entries from {file_name}:")
-        st.write(missing_keys)
-        return False
-    return True
-
-if not check_key_presence(medicines, 'name', 'medicines.json'):
-    st.stop()
-
 # Convert to dictionaries for easy lookup
-medicines_dict = {med['id']: med for med in medicines}
-symptoms_dict = {sym['id']: sym for sym in symptoms}
-indications_dict = {ind['id']: ind for ind in indications}
-diseases_dict = {dis['id']: dis for dis in diseases}
-side_effects_dict = {se['id']: se for se in side_effects}
-interactions_dict = {intc['id']: intc for intc in interactions}
-warnings_dict = {war['id']: war for war in warnings}
-contraindications_dict = {con['id']: con for con in contraindications}
-mechanisms_dict = {moa['id']: moa for moa in mechanisms_of_action}
-brand_names_dict = {bn['id']: bn for bn in brand_names}
-generic_names_dict = {gn['id']: gn for gn in generic_names}
-manufacturers_dict = {mf['id']: mf for mf in manufacturers}
+def to_dict(data, key='id'):
+    return {item[key]: item for item in data}
+
+medicines_dict = to_dict(medicines)
+symptoms_dict = to_dict(symptoms)
+indications_dict = to_dict(indications)
+diseases_dict = to_dict(diseases)
+side_effects_dict = to_dict(side_effects)
+interactions_dict = to_dict(interactions)
+warnings_dict = to_dict(warnings)
+contraindications_dict = to_dict(contraindications)
+mechanisms_dict = to_dict(mechanisms_of_action)
+brand_names_dict = to_dict(brand_names)
+generic_names_dict = to_dict(generic_names)
+manufacturers_dict = to_dict(manufacturers)
 
 # Function to find relationships
-def find_related_entities(medicine_id, relationship):
-    related_ids = [rel for rel in relationships[relationship] if rel['medicine_id'] == medicine_id]
+def find_related_entities(entity_id, relationship, entity_key='medicine_id'):
+    related_ids = [rel for rel in relationships[relationship] if rel[entity_key] == entity_id]
     return related_ids
+
+# Function to display related entities
+def display_related_entities(related_ids, entity_dict, entity_key):
+    for rel in related_ids:
+        st.write(entity_dict[rel[entity_key]]['name'])
 
 # Streamlit UI
 st.title('Medicine Knowledge Base')
 
-# Medicine Selection
-try:
+# Search Type Selection
+search_type = st.selectbox('Search by', ['Medicine', 'Symptom', 'Indication', 'Disease'])
+
+if search_type == 'Medicine':
+    # Medicine Selection
     medicine_options = {med['name']: med['id'] for med in medicines}
-except KeyError as e:
-    st.error(f"KeyError: {e}. Please make sure your JSON files have the correct structure.")
-    st.stop()
+    selected_medicine = st.selectbox('Select a Medicine', list(medicine_options.keys()))
+    
+    if selected_medicine:
+        medicine_id = medicine_options[selected_medicine]
+        st.write(f"**Description:** {medicines_dict[medicine_id]['description']}")
 
-selected_medicine = st.selectbox('Select a Medicine', list(medicine_options.keys()))
+        # Display related symptoms
+        st.subheader('Related Symptoms')
+        related_symptoms = find_related_entities(medicine_id, 'medicine_symptom')
+        display_related_entities(related_symptoms, symptoms_dict, 'symptom_id')
 
-if selected_medicine:
-    medicine_id = medicine_options[selected_medicine]
-    st.write(f"**Description:** {medicines_dict[medicine_id]['description']}")
+        # Display related indications
+        st.subheader('Related Indications')
+        related_indications = find_related_entities(medicine_id, 'medicine_indication')
+        display_related_entities(related_indications, indications_dict, 'indication_id')
 
-    # Display related symptoms
-    st.subheader('Related Symptoms')
-    related_symptoms = find_related_entities(medicine_id, 'medicine_symptom')
-    for rel in related_symptoms:
-        st.write(symptoms_dict[rel['symptom_id']]['name'])
+        # Display related diseases
+        st.subheader('Related Diseases')
+        related_diseases = find_related_entities(medicine_id, 'medicine_disease')
+        display_related_entities(related_diseases, diseases_dict, 'disease_id')
 
-    # Display related indications
-    st.subheader('Related Indications')
-    related_indications = find_related_entities(medicine_id, 'medicine_indication')
-    for rel in related_indications:
-        st.write(indications_dict[rel['indication_id']]['name'])
+        # Display related side effects
+        st.subheader('Related Side Effects')
+        related_side_effects = find_related_entities(medicine_id, 'medicine_side_effect')
+        display_related_entities(related_side_effects, side_effects_dict, 'side_effect_id')
 
-    # Display related diseases
-    st.subheader('Related Diseases')
-    related_diseases = find_related_entities(medicine_id, 'medicine_disease')
-    for rel in related_diseases:
-        st.write(diseases_dict[rel['disease_id']]['name'])
+        # Display related interactions
+        st.subheader('Related Interactions')
+        related_interactions = find_related_entities(medicine_id, 'medicine_interaction')
+        display_related_entities(related_interactions, interactions_dict, 'interaction_id')
 
-    # Display related side effects
-    st.subheader('Related Side Effects')
-    related_side_effects = find_related_entities(medicine_id, 'medicine_side_effect')
-    for rel in related_side_effects:
-        st.write(side_effects_dict[rel['side_effect_id']]['name'])
+        # Display related warnings
+        st.subheader('Related Warnings')
+        related_warnings = find_related_entities(medicine_id, 'medicine_warning')
+        display_related_entities(related_warnings, warnings_dict, 'warning_id')
 
-    # Display related interactions
-    st.subheader('Related Interactions')
-    related_interactions = find_related_entities(medicine_id, 'medicine_interaction')
-    for rel in related_interactions:
-        st.write(interactions_dict[rel['interaction_id']]['name'])
+        # Display related contraindications
+        st.subheader('Related Contraindications')
+        related_contraindications = find_related_entities(medicine_id, 'medicine_contraindication')
+        display_related_entities(related_contraindications, contraindications_dict, 'contraindication_id')
 
-    # Display related warnings
-    st.subheader('Related Warnings')
-    related_warnings = find_related_entities(medicine_id, 'medicine_warning')
-    for rel in related_warnings:
-        st.write(warnings_dict[rel['warning_id']]['name'])
+        # Display related mechanisms of action
+        st.subheader('Related Mechanisms of Action')
+        related_mechanisms = find_related_entities(medicine_id, 'medicine_mechanism')
+        display_related_entities(related_mechanisms, mechanisms_dict, 'mechanism_id')
 
-    # Display related contraindications
-    st.subheader('Related Contraindications')
-    related_contraindications = find_related_entities(medicine_id, 'medicine_contraindication')
-    for rel in related_contraindications:
-        st.write(contraindications_dict[rel['contraindication_id']]['name'])
+elif search_type == 'Symptom':
+    # Symptom Selection
+    symptom_options = {sym['name']: sym['id'] for sym in symptoms}
+    selected_symptom = st.selectbox('Select a Symptom', list(symptom_options.keys()))
 
-    # Display related mechanisms of action
-    st.subheader('Related Mechanisms of Action')
-    related_mechanisms = find_related_entities(medicine_id, 'medicine_mechanism')
-    for rel in related_mechanisms:
-        st.write(mechanisms_dict[rel['mechanism_id']]['description'])
+    if selected_symptom:
+        symptom_id = symptom_options[selected_symptom]
+        
+        # Display related medicines
+        st.subheader('Related Medicines')
+        related_medicines = find_related_entities(symptom_id, 'medicine_symptom', 'symptom_id')
+        display_related_entities(related_medicines, medicines_dict, 'medicine_id')
+
+elif search_type == 'Indication':
+    # Indication Selection
+    indication_options = {ind['name']: ind['id'] for ind in indications}
+    selected_indication = st.selectbox('Select an Indication', list(indication_options.keys()))
+
+    if selected_indication:
+        indication_id = indication_options[selected_indication]
+
+        # Display related medicines
+        st.subheader('Related Medicines')
+        related_medicines = find_related_entities(indication_id, 'medicine_indication', 'indication_id')
+        display_related_entities(related_medicines, medicines_dict, 'medicine_id')
+
+elif search_type == 'Disease':
+    # Disease Selection
+    disease_options = {dis['name']: dis['id'] for dis in diseases}
+    selected_disease = st.selectbox('Select a Disease', list(disease_options.keys()))
+
+    if selected_disease:
+        disease_id = disease_options[selected_disease]
+
+        # Display related medicines
+        st.subheader('Related Medicines')
+        related_medicines = find_related_entities(disease_id, 'medicine_disease', 'disease_id')
+        display_related_entities(related_medicines, medicines_dict, 'medicine_id')
 
 # Run the app
 if __name__ == '__main__':
